27bacbc34bd7f9001169cf77f21f650e1b7eb8e7
Nguyen Nguyên <nguyentunguyen08@gmail.com>
2024-01-20 09:39:25 +0700

    feat: update ui select

---
+++ src/components/UI/Select.tsx
@@ -0,0 +1,80 @@
+import { useClickOutSide } from "libs/useClickOutSide";
+import { useRef, useState } from "react";
+import styled from "styled-components";
+
+interface Props {
+  options: { label: string; value: any }[];
+  value: any;
+  onChange: (value: any) => void;
+}
+
+export const UISelect = ({ options, value, onChange }: Props) => {
+  const [isOpen, setOpen] = useState(false);
+  const ref: any = useRef();
+  useClickOutSide(ref, () => setOpen(false));
+
+  const opValue: any = options.find((op: any) => op.value == value) || {};
+  const handleOnChange = (v: any) => {
+    onChange(v);
+    setOpen(false);
+  };
+  return (
+    <Styles ref={ref} className="select-cover">
+      <div className="select-value" onClick={() => setOpen((prev) => !prev)}>
+        <span>{opValue.label || ""}</span>
+        <span>&#11206;</span>
+      </div>
+      {isOpen && (
+        <div className="select-popup">
+          {options.map((op: any) => (
+            <div
+              className="option-item"
+              onClick={() => handleOnChange(op.value)}
+            >
+              {op.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </Styles>
+  );
+};
+
+const Styles = styled.div`
+  position: relative;
+  width: 200px;
+
+  .select-value {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 8px 12px;
+    border: 1px solid #e3e8f5;
+    border-radius: 8px;
+    background: white;
+    cursor: pointer;
+  }
+
+  .select-popup {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: -4px;
+    transform: translateY(100%);
+    padding: 8px;
+    background: white;
+    box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
+    border-radius: 8px;
+    max-height: 200px;
+    overflow: auto;
+    > * {
+      padding: 4px;
+      cursor: pointer;
+      :hover {
+        color: blue;
+        background: rgba(0, 0, 0, 0.1);
+        border-radius: 2px;
+      }
+    }
+  }
+`;
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -3,6 +3,7 @@ import { UIFade } from "components/UI/Fade";
 import { UIList } from "components/UI/List";
 import { UISlider } from "components/UI/Slider";
 import { UIInput } from "components/UI/input/Input";
+import { UISelect } from "components/UI/Select";
 import { UIModal } from "components/UI/modal";
 import { importExcelFile } from "libs/file";
 import { useState } from "react";
@@ -11,8 +12,19 @@ import styled from "styled-components";
 export const HomeScreen = (props: any) => {
   const [num, setNum] = useState("");
   const [isOpen, setOpen] = useState(false);
+  const [select, setSelect] = useState("");
   return (
     <Styles>
+      <UISelect
+        options={Array(10)
+          .fill("")
+          .map((_, index: number) => ({
+            label: "Label" + index,
+            value: index,
+          }))}
+        value={select}
+        onChange={(v: any) => setSelect(v)}
+      />
       <UISlider
         items={Array(50).fill("")}
         slidesToShow={10}
